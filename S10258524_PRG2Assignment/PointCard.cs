ba9d605d9e2786a9808863518168dd93b59bbd86
yuhong000 <s10258441@connect.np.edu.sg>
2024-01-13 18:40:55 +0800

    testing

---
+++ S10258524_PRG2Assignment/PointCard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10258524_PRG2Assignment
+{
+    internal class PointCard
+    {
+        private int points;
+        private int punchCard;
+        private string tier;
+        public int Points { get; set; }
+        public int PunchCards { get; set; }
+        public string Tier { get; set; }
+
+        public PointCard() { }
+        public PointCard(int points, int punchCard) { points = Points; punchCard=PunchCards; }
+        public AddPoints
+    }
+}
