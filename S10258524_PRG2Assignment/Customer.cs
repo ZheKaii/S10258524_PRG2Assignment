1f88efae24baab0b634de0f6dc3eccdaf7bd8df4
yuhong000 <s10258441@connect.np.edu.sg>
2024-01-13 18:31:36 +0800

    tseting

---
+++ S10258524_PRG2Assignment/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10258524_PRG2Assignment
+{
+    internal class Customer
+    {
+        private string name;
+        private int memberId;
+        private DateTime dob;
+    }
+}
