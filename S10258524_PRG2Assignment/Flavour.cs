709f4b3663c690d004b5491d38f5f6e08993474b
ZheKaii <s10258524@connect.np.edu.sg>
2024-01-25 14:34:37 +0800

    clean up everything and add a menu

--- S10258524_PRG2Assignment/Flavour.cs
+++ S10258524_PRG2Assignment/Flavour.cs
@@ -16,14 +16,17 @@ namespace S10258524_PRG2Assignment
     {
         private string type;
         private bool premium;
+        private int quantity;
         public string Type { get; set; }
         public bool Premium { get; set; }
+        public int Quantity { get; set; }
         public Flavour() { }
         
-        public Flavour(string type, bool premium)
+        public Flavour(string type, bool premium, int quantity)
         {
             Type = type;
             Premium = premium;
+            Quantity = quantity;
         }
         public override string ToString()
         {
--- S10258524_PRG2Assignment/Program.cs
+++ S10258524_PRG2Assignment/Program.cs
@@ -14,122 +14,177 @@ namespace S10258524_PRG2Assignment
     {
         static void Main(string[] args)
         {
-            // Basic Feature 1 - Heng Zhe Kai
-            string[] all_line = File.ReadAllLines("customers.csv");
-            string[] headers = all_line[0].Split(",");
-            Console.WriteLine("{0,-11}{1,-10}{2,-23}{3,-18}{4,-18}{5,-11}",
-                headers[0], headers[1], headers[2], headers[3], headers[4], headers[5]);
+            int DisplayMenu()
+            {
+                Console.WriteLine("\n---------------- M E N U --------------------");
+                Console.WriteLine("[1] List information of all the customers");
+                Console.WriteLine("[2] List information of all current orders");
+                Console.WriteLine("[3] Register a new customer");
+                Console.WriteLine("[4] Create a customer's order");
+                Console.WriteLine("[5] Display order details of a customer");
+                Console.WriteLine("[6] Modify order details");
+                Console.WriteLine("[0] Exit");
+                Console.WriteLine("---------------------------------------------");
+                Console.Write("Enter your option: ");
+                int option = int.Parse(Console.ReadLine());
+                return option;
+            }
 
             List<Customer> customers = new List<Customer>();
-            for (int i = 1; i < all_line.Length; i++)
+            void Displayallcustomers()
             {
-                string[] cols = all_line[i].Split(",");
-                string name = cols[0];
-                int memberid = int.Parse(cols[1]);
-                DateTime dob = DateTime.ParseExact(cols[2], "dd/MM/yyyy", null);
-                string membershipstatus = cols[3];
-                int membershippoints = int.Parse(cols[4]);
-                int punchcard = int.Parse(cols[5]);
-                PointCard pointCard = new PointCard(membershippoints, punchcard, membershipstatus);
-                Customer customer = new Customer(name, memberid, dob);
-                customer.Rewards = pointCard;
-                customers.Add(customer);
-            }
-            for (int i = 0; i < customers.Count; i++)
-            {
-                Customer c = customers[i];
-                Console.WriteLine("{0,-11}{1,-10}{2,-23}{3,-18}{4,-18}{5,-11}", c.Name, c.MemberId, c.Dob, c.Rewards.Tier, c.Rewards.Points, c.Rewards.PunchCards);
-            }
-            Console.WriteLine();
+                // Basic Feature 1 - Heng Zhe Kai
+                string[] all_line = File.ReadAllLines("customers.csv");
+                string[] headers = all_line[0].Split(",");
+                Console.WriteLine("{0,-11}{1,-10}{2,-23}{3,-18}{4,-18}{5,-11}",
+                    headers[0], headers[1], headers[2], headers[3], headers[4], headers[5]);
 
-            // Basic Feature 3 - Heng Zhe Kai
-            Console.Write("Enter your name: ");
-            string customername = Console.ReadLine();
-            Console.Write("Enter your ID number: ");
-            int customerid = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter your date of birth: ");
-            DateTime customerdob = Convert.ToDateTime(Console.ReadLine());
-            Customer c1 = new Customer(customername, customerid, customerdob);
-            PointCard p1 = new PointCard(0, 0, "Ordinary");
-            c1.Rewards = p1;
-            using (StreamWriter sw = new StreamWriter("customers.csv", true))
-            {
-                sw.WriteLine($"{c1.Name},{c1.MemberId},{c1.Dob},{c1.Rewards.Tier},{c1.Rewards.Points},{c1.Rewards.PunchCards}");
-            }
-            Console.WriteLine("You have successfully registered as a membership in our system!");
-            Console.WriteLine();
-
-            // Basic Feature 4 - Heng Zhe Kai
-            for (int i = 0; i < customers.Count;i++)
-            {
-                Customer c = customers[i];
-                Console.WriteLine("{0,-11}{1,-10}{2,-23}{3,-18}{4,-18}{5,-11}", c.Name, c.MemberId, c.Dob, c.Rewards.Tier, c.Rewards.Points, c.Rewards.PunchCards);
-            }
-            Customer? Search(List<Customer> customerslist, string orderingcustomer)
-            {
-                foreach(Customer customer in customerslist)
+                for (int i = 1; i < all_line.Length; i++)
                 {
-                    if (customer.Name == orderingcustomer)
-                    {
-                        return customer;
-                    }
+                    string[] cols = all_line[i].Split(",");
+                    string name = cols[0];
+                    int memberid = int.Parse(cols[1]);
+                    DateTime dob = DateTime.ParseExact(cols[2], "dd/MM/yyyy", null);
+                    string membershipstatus = cols[3];
+                    int membershippoints = int.Parse(cols[4]);
+                    int punchcard = int.Parse(cols[5]);
+                    PointCard pointCard = new PointCard(membershippoints, punchcard, membershipstatus);
+                    Customer customer = new Customer(name, memberid, dob);
+                    customer.Rewards = pointCard;
+                    customers.Add(customer);
                 }
-                return null;
-            }
-            Console.WriteLine("Please select a customer from the list: ");
-            string orderingcustomer = Console.ReadLine();
-            Customer foundcustomername = Search(customers, orderingcustomer);
-            if (foundcustomername == null)
-            {
-                Console.WriteLine("Unable to find the customer name. Please try again.");
-            }
-            else
-            {
-                Console.Write("Enter your ice cream option: ");
-                string option = Console.ReadLine();
-                Console.WriteLine("Enter the number of scoops: ");
-                int scoops = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter your flavour(s): ");
-                string type = Console.ReadLine();
-                List<Flavour> flavours = new List<Flavour>();
-                Console.WriteLine("Enter your topping(s): ");
-                string topping = Console.ReadLine();
-                List<Topping> toppings = new List<Topping>();
+                for (int i = 0; i < customers.Count; i++)
+                {
+                    Customer c = customers[i];
+                    Console.WriteLine("{0,-11}{1,-10}{2,-23}{3,-18}{4,-18}{5,-11}", c.Name, c.MemberId, c.Dob, c.Rewards.Tier, c.Rewards.Points, c.Rewards.PunchCards);
+                }
+                Console.WriteLine();
             }
 
-            // For Yu Hong to do his features
-            Main2(args);
-        }
-
-        //Basic Feature 2 - Gan Yu Hong
-        static void Main2(string[] args)
-        {
-            string[] all_line = File.ReadAllLines("orders.csv");
-            string[] headers = all_line[0].Split(",");
-            Console.WriteLine("{0,-2} {1,-8} {2,-22} {3,-22} {4,-6} {5,-6} {6,-6} {7,-14} {8,-10} {9,-10} {10,-10} {11,-10} {12,-10} {13,-10} {14,-10}",
-                headers[0], headers[1], headers[2], headers[3], headers[4], headers[5], headers[6], headers[7],
-                headers[8], headers[9], headers[10], headers[11], headers[12], headers[13], headers[14]);
             List<Order> orders = new List<Order>();
-            for (int i = 1;  i < all_line.Length; i++)
+            void Displayorders()
             {
-                string[] cols = all_line[i].Split(",");
-                int id = int.Parse(cols[0]);
-                int memberid = int.Parse(cols[1]);
-                DateTime timeReceived = DateTime.ParseExact(cols[2], "dd/MM/yyyy HH:mm", null);
-                DateTime? timefulfilled = DateTime.ParseExact(cols[3], "dd/MM/yyyy HH:mm", null);
-                string option = cols[4];
-                int scoops = int.Parse(cols[5]);
-                string dipped = cols[6];
-                string waffleflavour = cols[7];
-                string flavour1 = cols[8];
-                string flavour2 = cols[9];
-                string flavour3 = cols[10];
-                string topping1 = cols[11];
-                string topping2 = cols[12];
-                string topping3 = cols[13];
-                string topping4 = cols[14];
+                // Basic Feature 2 - Gan Yu Hong
+                string[] all_line = File.ReadAllLines("orders.csv");
+                string[] headers = all_line[0].Split(",");
                 Console.WriteLine("{0,-2} {1,-8} {2,-22} {3,-22} {4,-6} {5,-6} {6,-6} {7,-14} {8,-10} {9,-10} {10,-10} {11,-10} {12,-10} {13,-10} {14,-10}",
-                id, memberid, timeReceived, timefulfilled, option, scoops , dipped, waffleflavour, flavour1, flavour2, flavour3, topping1, topping2, topping3, topping4);
+                    headers[0], headers[1], headers[2], headers[3], headers[4], headers[5], headers[6], headers[7],
+                    headers[8], headers[9], headers[10], headers[11], headers[12], headers[13], headers[14]);
+                for (int i = 1; i < all_line.Length; i++)
+                {
+                    string[] cols = all_line[i].Split(",");
+                    int id = int.Parse(cols[0]);
+                    int memberid = int.Parse(cols[1]);
+                    DateTime timeReceived = DateTime.ParseExact(cols[2], "dd/MM/yyyy HH:mm", null);
+                    DateTime? timefulfilled = DateTime.ParseExact(cols[3], "dd/MM/yyyy HH:mm", null);
+                    string option = cols[4];
+                    int scoops = int.Parse(cols[5]);
+                    string dipped = cols[6];
+                    string waffleflavour = cols[7];
+                    string flavour1 = cols[8];
+                    string flavour2 = cols[9];
+                    string flavour3 = cols[10];
+                    string topping1 = cols[11];
+                    string topping2 = cols[12];
+                    string topping3 = cols[13];
+                    string topping4 = cols[14];
+                    Console.WriteLine("{0,-2} {1,-8} {2,-22} {3,-22} {4,-6} {5,-6} {6,-6} {7,-14} {8,-10} {9,-10} {10,-10} {11,-10} {12,-10} {13,-10} {14,-10}",
+                    id, memberid, timeReceived, timefulfilled, option, scoops, dipped, waffleflavour, flavour1, flavour2, flavour3, topping1, topping2, topping3, topping4);
+                }
+                Console.WriteLine();
+            }
+
+            void Registercustomer()
+            {
+                // Basic Feature 3 - Heng Zhe Kai
+                Console.Write("Enter your name: ");
+                string customername = Console.ReadLine();
+                Console.Write("Enter your ID number: ");
+                int customerid = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter your date of birth: ");
+                DateTime customerdob = Convert.ToDateTime(Console.ReadLine());
+                Customer c1 = new Customer(customername, customerid, customerdob);
+                PointCard p1 = new PointCard(0, 0, "Ordinary");
+                c1.Rewards = p1;
+                using (StreamWriter sw = new StreamWriter("customers.csv", true))
+                {
+                    sw.WriteLine($"{c1.Name},{c1.MemberId},{c1.Dob},{c1.Rewards.Tier},{c1.Rewards.Points},{c1.Rewards.PunchCards}");
+                }
+                Console.WriteLine("You have successfully registered as a membership in our system!");
+                Console.WriteLine();
+            }
+
+            void Createcustomerorder()
+            {
+                // Basic Feature 4 - Heng Zhe Kai
+                for (int i = 0; i < customers.Count; i++)
+                {
+                    Customer c = customers[i];
+                    Console.WriteLine("{0,-11}{1,-10}{2,-23}{3,-18}{4,-18}{5,-11}", c.Name, c.MemberId, c.Dob, c.Rewards.Tier, c.Rewards.Points, c.Rewards.PunchCards);
+                }
+                Customer? Search(List<Customer> customerslist, string orderingcustomer)
+                {
+                    foreach (Customer customer in customerslist)
+                    {
+                        if (customer.Name == orderingcustomer)
+                        {
+                            return customer;
+                        }
+                    }
+                    return null;
+                }
+                Console.WriteLine("Please select a customer from the list: ");
+                string orderingcustomer = Console.ReadLine();
+                Customer foundcustomername = Search(customers, orderingcustomer);
+                if (foundcustomername == null)
+                {
+                    Console.WriteLine("Unable to find the customer name. Please try again.");
+                }
+                else
+                {
+                    Console.Write("Enter your ice cream option: ");
+                    string option = Console.ReadLine();
+                    Console.WriteLine("Enter the number of scoops: ");
+                    int scoops = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter your flavour(s): ");
+                    string type = Console.ReadLine();
+                    List<Flavour> flavours = new List<Flavour>();
+                    Console.WriteLine("Enter your topping(s): ");
+                    string topping = Console.ReadLine();
+                    List<Topping> toppings = new List<Topping>();
+                }
+            }
+
+            while (true)
+            {
+                int option = DisplayMenu();
+                if (option == 0)
+                {
+                    Console.WriteLine("\n---------");
+                    Console.WriteLine("Goodbye!");
+                    Console.WriteLine("---------");
+                    break;
+                }
+                else if (option == 1)
+                {
+                    Displayallcustomers();
+                }
+                else if (option == 2)
+                {
+                    Displayorders();
+                }
+                else if (option == 3)
+                {
+                    Registercustomer();
+                }
+                else if (option == 4)
+                {
+                    Createcustomerorder();
+                }
+                else
+                {
+                    Console.WriteLine("Please choose another option.");
+                }
             }
         }
     }
